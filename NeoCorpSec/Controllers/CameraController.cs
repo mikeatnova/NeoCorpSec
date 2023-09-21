b5b705fede817af875182d69ba943d1f1e309c1a
Mike Cognata <117309850+mikeatnova@users.noreply.github.com>
2023-09-21 15:52:23 -0400

    + Created the base for the Camera List page

--- NeoCorpSec/Controllers/CameraController.cs
+++ NeoCorpSec/Controllers/CameraController.cs
@@ -1,9 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using NeoCorpSec.Services;
+using System.Text;
+using System.Text.Json;
 
 namespace NeoCorpSec.Controllers
 {
-    public class CameraController : Controller
+    [Authorize(Roles = "Neo, SecurityChief, SecurityManager, SecuritySupervisor, SecurityOfficer")]
+    public class CameraController : CoreController
     {
+        private readonly IConfiguration _configuration;
+        private readonly JwtExtractorHelper _jwtExtractorHelper;
+
+        public CameraController(IConfiguration configuration, JwtExtractorHelper jwtExtractorHelper)
+            : base() // Call the base constructor with the required parameter
+        {
+            _jwtExtractorHelper = jwtExtractorHelper;
+            _configuration = configuration;
+        }
+
+        private async Task<IActionResult> GetViewAsync<T>(string url)
+        {
+            using (var httpClient = InitializeHttpClient())
+            {
+                var response = await httpClient.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var options = new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    };
+                    var items = JsonSerializer.Deserialize<List<T>>(content, options);
+                    return View(items);
+                }
+            }
+            return View(new List<T>()); // Return an empty list if the request fails
+        }
+
         public IActionResult Index()
         {
             return View();
--- NeoCorpSec/Controllers/HomeController.cs
+++ NeoCorpSec/Controllers/HomeController.cs
@@ -1,18 +1,44 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NeoCorpSec.Models;
+using NeoCorpSec.Services;
 using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
 
 namespace NeoCorpSec.Controllers
 {
     [Authorize(Roles = "Neo, SecurityChief, SecurityManager, SecuritySupervisor, SecurityOfficer")]
     public class HomeController : CoreController
     {
+        private readonly IConfiguration _configuration;
+        private readonly JwtExtractorHelper _jwtExtractorHelper;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration, JwtExtractorHelper jwtExtractorHelper)
         {
             _logger = logger;
+            _jwtExtractorHelper = jwtExtractorHelper;
+            _configuration = configuration;
+        }
+
+        private async Task<IActionResult> GetViewAsync<T>(string url)
+        {
+            using (var httpClient = InitializeHttpClient())
+            {
+                var response = await httpClient.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var options = new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    };
+                    var items = JsonSerializer.Deserialize<List<T>>(content, options);
+                    return View(items);
+                }
+            }
+            return View(new List<T>());
         }
 
         public IActionResult Index()
@@ -25,9 +51,57 @@ namespace NeoCorpSec.Controllers
             return View();
         }
 
-        public IActionResult CameraList()
+        [HttpGet]
+        public async Task<IActionResult> CameraList()
         {
-            return View();
+            string baseUrl = _configuration.GetValue<string>("NeoNovaApiBaseUrl");
+            using (var httpClient = InitializeHttpClient())
+            {
+                // Fetch Cameras
+                var cameraResponse = await httpClient.GetAsync($"{baseUrl}/api/Camera");
+
+                // Fetch Locations
+                var locationResponse = await httpClient.GetAsync($"{baseUrl}/api/Location");
+
+                if (cameraResponse.IsSuccessStatusCode && locationResponse.IsSuccessStatusCode)
+                {
+                    var cameraContent = await cameraResponse.Content.ReadAsStringAsync();
+                    var locationContent = await locationResponse.Content.ReadAsStringAsync();
+
+                    var cameraOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+                    var locationOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+                    var cameras = JsonSerializer.Deserialize<List<Models.CameraManagment.Camera>>(cameraContent, cameraOptions);
+                    var locations = JsonSerializer.Deserialize<List<Models.CameraManagment.Location>>(locationContent, locationOptions);
+
+                    // Map LocationId to Location objects
+                    ViewBag.LocationMap = locations.ToDictionary(l => l.ID); // O(N)
+
+
+                    return View(cameras);
+                }
+            }
+            return View(new List<Models.CameraManagment.Camera>());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PutCamera(int id, Models.CameraManagment.Camera camera)
+        {
+            using (var httpClient = InitializeHttpClient())
+            {
+                string baseUrl = _configuration.GetValue<string>("NeoNovaApiBaseUrl");
+                camera.ModifiedAt = DateTime.UtcNow;
+                var json = JsonSerializer.Serialize(camera);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await httpClient.PutAsync($"{baseUrl}/api/Camera/{id}", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    // Redirect to the GET action to refresh the page
+                    return RedirectToAction("CameraList");
+                }
+            }
+            // Handle error, e.g., return a view with an error message
+            return View("Error");
         }
 
         public IActionResult Reports()
