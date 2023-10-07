c5368ce5bd778af52310f57be3276e6c668cab98
Mike Cognata <117309850+mikeatnova@users.noreply.github.com>
2023-10-07 15:17:28 -0400

    + added base for universal messagin system portal

---
+++ NeoCorpSec/Models/Messaging/PalantirMessage.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NeoCorpSec.Models.Messaging
+{
+    public class PalantirMessage
+    {
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string MessageBody { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Status { get; set; }
+
+        [Required]
+        public string Realm { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int UrgencyRating { get; set; }
+    }
+}
