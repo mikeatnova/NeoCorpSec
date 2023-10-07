d29caf116aa918339bab92778d0815a64c5cbe01
Mike Cognata <117309850+mikeatnova@users.noreply.github.com>
2023-10-07 10:34:01 -0400

    + admin work, add user-edit user tighten up

---
+++ NeoCorpSec/Models/Authenitcation/AdminAddSecurityUser.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NeoCorpSec.Models.Authenitcation
+{
+    public class AdminAddSecurityUser
+    {
+        [MaxLength(50)]
+        public string? FirstName { get; set; } // From Security User Model
+
+        [MaxLength(50)]
+        public string? LastName { get; set; } // From Security User Model
+
+        //[Required]
+        [EmailAddress]
+        public string? Email { get; set; } // From ASpNetUsers
+
+        [Required]
+        public required string Role { get; set; } // From ASpNetUsers, AspNetRoles, AspNetUserRoles
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        public required string Password { get; set; } // From ASpNetUsers
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // From Security User Model
+
+        public DateTime? HiredDate { get; set; } // From Security User Model
+        public string? PhoneNumber { get; set; } // From ASpNetUsers
+    }
+}
--- NeoCorpSec/Models/Authenitcation/AdminCombinedSecurityUser.cs
+++ NeoCorpSec/Models/Authenitcation/AdminCombinedSecurityUser.cs
@@ -8,6 +8,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string HiredDate { get; set; }
+        public string PhoneNumber { get; set; }
         public List<string> Roles { get; set; }
 
         public AdminCombinedSecurityUser()
